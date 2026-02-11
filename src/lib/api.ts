d9d658fc84d7704c1ab6eeebdaee5b269e773cd5
WallsDevs <teamwallsdevs@gmail.com>
2026-02-11 16:02:45 -0400

    fix(api): Simplificar headers de axios para evitar conflictos CORS

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -21,8 +21,6 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
-    "Access-Control-Allow-Origin": "*", // Permitir cualquier origen
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
   },
 });
 
