e29b1ec689e650dd527f2d1b5fc771b3509554ad
WallsDevs <teamwallsdevs@gmail.com>
2026-02-12 11:39:50 -0400

    feat register

--- src/app/(public)/registro/page.tsx
+++ src/app/(public)/registro/page.tsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import RegistroForm from "./RegistroForm";
 import styles from "../login/loginPage.module.css";
 import { register, updateUserName } from "@/services/auth";
-import { updateUserRole, getClientRoleId } from "@/services/users";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 export default function RegistroPage() {
@@ -37,11 +36,6 @@ export default function RegistroPage() {
       if (userId && jwt) {
         // Actualizar el nombre del usuario
         await updateUserName(userId, data.nombre, jwt);
-        
-        // Asignar el rol de cliente (obteniendo el ID dinámicamente)
-        // Este paso asegura que el usuario tenga siempre el rol correcto
-        const clientRoleId = await getClientRoleId();
-        await updateUserRole(userId, clientRoleId);
       }
       toast.success("¡Registro exitoso!");
       router.push("/login");
